3907e19ba4bfe0f4dbad93ec24bd277d99a97649
Ed-Chris <147745943+Ed-Chris@users.noreply.github.com>
2024-07-19 20:14:38 -0600

    Update page_08.py

--- pages/page_08.py
+++ pages/page_08.py
@@ -54,6 +54,8 @@ bar_chart.update_layout(
         xanchor="center",
         x=0.5
     ),
+    width=1000,  # Set the width of the chart
+    height=600,  # Set the height of the chart
     yaxis_title='Wage',
 )
 
